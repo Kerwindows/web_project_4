8761394e93cbf4ec890d0ddc96fa69c87a9d937e
Kerwindows <kerwindows@hotmail.com>
2022-07-22 07:36:28 -0400

    added files to src folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,82 +1,29 @@
 import "./index.css";
-
-import FormValidator from "../../components/form-validator.js";
-import Card from "../../components/card.js";
-import Section from "../../components/section.js";
-import PopupWithImage from "../../components/popup-with-image.js";
-import PopupWithForm from "../../components/popup-with-form.js";
-import UserInfo from "../../components/UserInfo.js";
-
-const editProfileOpenBtn = document.querySelector(".profile__edit-btn");
-const profileNameInput = document.querySelector(".profile__edit-name");
-const profileOccupationInput = document.querySelector(".profile__about-me");
-const profileForm = document.querySelector("#edit__profile");
-const editProfileCloseBtn = document.querySelector(".popup__edit-close-btn");
-const popupProfileName = document.querySelector(".js-input-type-profile-name");
-const popupProfileIconsTitle = document.querySelector(
-  ".js-input-type-profile-about-me"
-);
-const submitProfileEdit = document.querySelector(".popup__edit-form");
-/* -------------------------------- add place ------------------------------- */
-const addPlaceForm = document.forms.addPlaceForm;
-const popupAddPlaceForm = document.querySelector("#add-place-popup");
-const addPlacesOpenBtn = document.querySelector(".profile__add-places-btn");
-const addPlaceCloseBtn = document.querySelector(".popup__place-close-btn");
-const popupPlaceName = document.querySelector(".js-input-type-place-name");
-const popupPlaceUrl = document.querySelector(".js-input-type-place-url");
-const submitNewPlace = document.querySelector(".popup__place-form");
-/* ----------------------------- Generate Cards ----------------------------- */
-const placeList = document.querySelector(".cards__list");
-/* ------------------------------ image preview ----------------------------- */
-const imagePopup = document.querySelector("#view__image");
-const viewImageCloseBtn = document.querySelector(".popup__image-close-btn");
-
-const addPopupSelector = "#add-place-popup";
-const editPopupSelector = "#edit__profile";
-const profileName = ".profile__edit-name";
-const profileJob = ".profile__about-me";
-
-const initialPlaces = [
-  {
-    name: "Tobago",
-    link:
-      "https://th.bing.com/th/id/OIP.AfQeN6j8IHA1QwQV1LAhMgHaE8?pid=ImgDet&rs=1"
-  },
-  {
-    name: "Turks & Caicos",
-    link:
-      "https://th.bing.com/th/id/R.2256ea7e2645811623722e1984fa2cea?rik=Pul7OZw45sq3Ig&pid=ImgRaw&r=0"
-  },
-  {
-    name: "Paris",
-    link:
-      "https://th.bing.com/th/id/R.9550f55006740bf41c970da21eee7bad?rik=6890JkFG%2f4Cs2w&pid=ImgRaw&r=0"
-  },
-  {
-    name: "South Island, New Zealand",
-    link:
-      "https://th.bing.com/th/id/OIP.eda4bswSTGR8dkvPL382IgHaEt?pid=ImgDet&rs=1"
-  },
-  {
-    name: "West Maui",
-    link:
-      "https://th.bing.com/th/id/OIP.H1kCOg0IQXbEx3p9k-DXkAHaE5?pid=ImgDet&rs=1"
-  },
-  {
-    name: "Orlando, Florida",
-    link:
-      "https://images.unsplash.com/photo-1597466599360-3b9775841aec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
-  }
-];
-
-const validationConfig = {
-  formSelector: ".popup__form",
-  inputSelector: ".popup__form-input",
-  submitButtonSelector: ".popup__button",
-  inactiveButtonClass: "popup__button_disabled",
-  inputErrorClass: "popup__input-type-error",
-  errorClass: "popup__error_visible"
-};
+import FormValidator from "../components/FormValidator.js";
+import Card from "../components/Card.js";
+import Section from "../components/Section.js";
+import PopupWithImage from "../components/PopupWithImage.js";
+import PopupWithForm from "../components/PopupWithForm.js";
+import UserInfo from "../components/UserInfo.js";
+import {
+  initialPlaces,
+  placeList,
+  addPopupSelector,
+  editProfileOpenBtn,
+  profileNameInput,
+  profileOccupationInput,
+  popupProfileName,
+  popupProfileIconsTitle,
+  submitProfileEdit,
+  addPlacesOpenBtn,
+  popupPlaceName,
+  popupPlaceUrl,
+  submitNewPlace,
+  editPopupSelector,
+  profileName,
+  profileJob,
+  validationConfig
+} from "../utils/constants.js";
 
 /* --------------------------------- function for rendering card on load --------------------------------- */
 
@@ -165,19 +112,12 @@ const editFormPopup = new PopupWithForm(editPopupSelector, {
   }
 });
 editFormPopup.setEventListeners();
-// editProfileOpenBtn.addEventListener("click", () => {
-//   // editFormPopup.open();
-//   //editFormPopup.setInputValues(newUserInfo.getUserInfo());
-
-//   
-// });
 
 editProfileOpenBtn.addEventListener("click", () => {
   editFormPopup.open();
 });
 
 submitProfileEdit.addEventListener("submit", submitEditProfileForm);
-
 
 /* --------------------------------- Verification --------------------------------- */
 const profileFormValidator = new FormValidator(
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,28 +1,24 @@
 export const editProfileOpenBtn = document.querySelector(".profile__edit-btn");
-export const profileName = document.querySelector(".profile__edit-name");
-export const profileIconsTitle = document.querySelector(".profile__about-me");
-export const profileForm = document.querySelector("#edit__profile");
-export const editProfileCloseBtn = document.querySelector(".popup__edit-close-btn");
+export const profileNameInput = document.querySelector(".profile__edit-name");
+export const profileOccupationInput = document.querySelector(".profile__about-me");
 export const popupProfileName = document.querySelector(".js-input-type-profile-name");
 export const popupProfileIconsTitle = document.querySelector(
   ".js-input-type-profile-about-me"
 );
 export const submitProfileEdit = document.querySelector(".popup__edit-form");
 /* -------------------------------- add place ------------------------------- */
-export const addPlaceForm = document.forms.addPlaceForm;
-export const popupAddPlaceForm = document.querySelector("#add-place-popup");
 export const addPlacesOpenBtn = document.querySelector(".profile__add-places-btn");
-export const addPlaceCloseBtn = document.querySelector(".popup__place-close-btn");
 export const popupPlaceName = document.querySelector(".js-input-type-place-name");
 export const popupPlaceUrl = document.querySelector(".js-input-type-place-url");
 export const submitNewPlace = document.querySelector(".popup__place-form");
 /* ----------------------------- Generate Cards ----------------------------- */
 export const placeList = document.querySelector(".cards__list");
 /* ------------------------------ image preview ----------------------------- */
-export const imagePopup = document.querySelector("#view__image");
-export const viewImageCloseBtn = document.querySelector(".popup__image-close-btn");
 
 export const addPopupSelector = "#add-place-popup";
+export const editPopupSelector = "#edit__profile";
+export const profileName = ".profile__edit-name";
+export const profileJob = ".profile__about-me";
 
 export const initialPlaces = [
   {
@@ -65,4 +61,3 @@ export const validationConfig = {
   inputErrorClass: "popup__input-type-error",
   errorClass: "popup__error_visible"
 };
-
